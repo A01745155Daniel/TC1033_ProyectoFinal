30fb6e4b3111a00e1829670084fe18ef1f4c7afd
A01745155 <135541990+A01745155Daniel@users.noreply.github.com>
2023-06-06 09:42:16 -0600

    Hola mundo

---
+++ Prueba.cpp
@@ -0,0 +1,11 @@
+/*
+Daniel Guerrero González A01745155
+*/
+#include <iostream>
+using namespace std;
+
+int main (int argc, char const *argv[])
+{
+    cout << "Hola mundo" << endl;
+    return 0;
+}
